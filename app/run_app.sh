c3c8790431fe087819c2aac2dc2563773c31e997
Spasov <i.spasov@sap.com>
2020-09-18 13:57:56 +0300

    Run Script v1

--- app/run_app.sh
+++ app/run_app.sh
@@ -6,4 +6,4 @@ export DOCKER_HOST=127.0.0.1:2375
 
 # if we want to use docker-compose without sudo, we need to add docker to group????
 # https://stackoverflow.com/questions/29101043/cant-connect-to-docker-from-docker-compose
-sudo docker-compose up -d
+docker-compose up -d
