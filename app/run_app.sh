d3279dec4d8161f40e31b8cfe6a1e055db5a6868
Spasov <i.spasov@sap.com>
2020-09-18 13:46:38 +0300

    Added Sudo to docker command

--- app/run_app.sh
+++ app/run_app.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 cd ./front-end-spring-boot/app
-docker-compose up -d
+
+# if we want to use docker-compose without sudo, we need to add docker to group????
+# https://stackoverflow.com/questions/29101043/cant-connect-to-docker-from-docker-compose
+sudo docker-compose up -d
